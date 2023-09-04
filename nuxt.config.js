bb9eef8e2ada7e316a820e8a21f4b67a01cd3924
mahmoudElzanklony <mahmoud_elzanklony@yahoo.com>
2023-09-04 20:54:57 +0300

    add

--- nuxt.config.js
+++ nuxt.config.js
@@ -80,7 +80,8 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://localhost:8000/api/',
+    // baseURL: 'http://localhost:8000/api/',
+     baseURL: 'https://jd-api.skillar.com/api',
     // proxy: true,
   },
 
